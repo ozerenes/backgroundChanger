6710a81455aa2015d890d2837c94298d54f22f00
enesozer <enesozer@yahoo.com>
2021-11-27 22:43:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    document.body.style.background = randomColor();
+});
+
+function randomColor() {
+    return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+}
